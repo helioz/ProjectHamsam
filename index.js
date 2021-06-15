bb3d826c467b8228634f30508f227c328e82bab2
Soorej Jones <soorejjones@gmail.com>
2021-06-15 23:13:41 +0530

    adding healthcheck API

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const express = require('express');
 const morgan = require('morgan');
 const Env = require('./config/environment');
-const session = require('./config/session.js');
+const Mongo = require('./lib/mongoDB');
+const Logger = require('./lib/standardLogging')
+const StandardError = require('./lib/standardError');
+
+
+// const session = require('./config/session.js');
 
 // Db setup
-const connection = require('./config/database');
+// const connection = require('./config/database');
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // Session management
-app.use(session);
+// app.use(session);
 
 app.get('/', (req, res, next) => {
     res.send('<h1> Hello World </h1>');
 });
+
+app.get('/healthcheck', async (req, res, next) => {
+    try {
+        const dbConn = await Mongo.getDbClient();
+        const resp = await dbConn.collection('userInfo').findOne({ "name": 'aloha' });
+        Logger.info('healthcheck', 'index', resp)
+        res.status(200).send({ status: 'healthy' });
+        return;
+    } catch (error) {
+        Logger.error('healthcheck', 'index', 'DB connection unhealthy', error);
+        res.status(500).send(StandardError(500, 'DB_CONN_UNHEALTHY', 'DB connection unhealthy', { allowed: true }));
+        return;
+    }
+})
 
 app.listen(Env.Port || '3000');
